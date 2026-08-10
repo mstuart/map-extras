50ae61753260229d736604dee66e3fb65652ea0c
Mark Stuart <mstuart@users.noreply.github.com>
2026-08-09 21:08:35 -0700

    chore: enforce ultracite

--- index.js
+++ index.js
@@ -1,53 +1,53 @@
 export function mapValues(map, function_) {
-	const result = new Map();
-	for (const [key, value] of map) {
-		result.set(key, function_(value, key));
-	}
+  const result = new Map();
+  for (const [key, value] of map) {
+    result.set(key, function_(value, key));
+  }
 
-	return result;
+  return result;
 }
 
 export function filterEntries(map, shouldKeep) {
-	const result = new Map();
-	for (const [key, value] of map) {
-		if (shouldKeep(value, key)) {
-			result.set(key, value);
-		}
-	}
+  const result = new Map();
+  for (const [key, value] of map) {
+    if (shouldKeep(value, key)) {
+      result.set(key, value);
+    }
+  }
 
-	return result;
+  return result;
 }
 
 export function mergeMap(...maps) {
-	const result = new Map();
-	for (const map of maps) {
-		for (const [key, value] of map) {
-			result.set(key, value);
-		}
-	}
+  const result = new Map();
+  for (const map of maps) {
+    for (const [key, value] of map) {
+      result.set(key, value);
+    }
+  }
 
-	return result;
+  return result;
 }
 
 export function invertMap(map) {
-	const result = new Map();
-	for (const [key, value] of map) {
-		result.set(value, key);
-	}
+  const result = new Map();
+  for (const [key, value] of map) {
+    result.set(value, key);
+  }
 
-	return result;
+  return result;
 }
 
 export function groupBy(iterable, function_) {
-	const result = new Map();
-	for (const item of iterable) {
-		const key = function_(item);
-		if (!result.has(key)) {
-			result.set(key, []);
-		}
+  const result = new Map();
+  for (const item of iterable) {
+    const key = function_(item);
+    if (!result.has(key)) {
+      result.set(key, []);
+    }
 
-		result.get(key).push(item);
-	}
+    result.get(key).push(item);
+  }
 
-	return result;
+  return result;
 }
